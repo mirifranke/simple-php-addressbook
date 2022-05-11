607503e60f9e691aefb9ce70e15c8b9afa24ec1d
Miriam Franke <miriam.franke77@gmail.com>
2022-05-11 12:56:51 +0200

    change column types

--- database/migrations/create_contacts_table.sql
+++ database/migrations/create_contacts_table.sql
@@ -1,11 +1,11 @@
 CREATE TABLE `contacts` (
     `id` bigint NOT NULL AUTO_INCREMENT,
-    `last_name` varchar(255) NOT NULL,
-    `first_name` varchar(255),
-    `street` varchar(255),
-    `zip` varchar(255),
-    `city` varchar(255),
-    `phone_number` varchar(255),
+    `last_name` varchar(20) NOT NULL,
+    `first_name` varchar(20),
+    `street` varchar(40),
+    `zip` varchar(8),
+    `city` varchar(30),
+    `phone_number` varchar(30),
     `created_at` timestamp,
     `updated_at` timestamp,
     PRIMARY KEY (`id`)
